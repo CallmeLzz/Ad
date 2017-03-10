6f5a3f3790aca8c8ccd76dadf72758f185dcfbdb
Nhân Võ <tannhan113@gmail.com>
2017-03-10 13:11:24 +0700

    Delete Sample
    Delete Sample

--- src/controllers/SampleAdminController.php
+++ src/controllers/SampleAdminController.php
@@ -4,6 +4,9 @@ namespace Source\Ad\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use URL;
+use Route,
+    Redirect;
 use Source\Ad\Models\Samples;
 
 Class SampleAdminController extends Controller
@@ -29,5 +32,29 @@ Class SampleAdminController extends Controller
         return view('ad::admin.sample.sample_index', $this->data_view);
     }
 
+     public function delete(Request $request) {
+
+        $sample = NULL;
+        $sample_id = $request->get('id');
+
+        if (!empty($sample_id)) {
+            $sample = $this->obj_sample->find($sample_id);
+
+            if (!empty($sample)) {
+                  //Message
+                // \Session::flash('message', trans('sample::sample_admin.message_delete_successfully'));
+
+                $sample->delete();
+            }
+        } else {
+
+        }
+        
+        $this->data_view = array_merge($this->data_view, array(
+            'samples' => $sample,
+        ));
+
+        return Redirect::route("admin.sample");
+    }
     
 }
