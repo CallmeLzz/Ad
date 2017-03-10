87e7ddc1e5bce06223d32130f3615b69d598060e
nhutran96 <tranvannhu96@gmail.com>
2017-03-10 13:21:42 +0700

    delete

--- src/controllers/SampleAdminController.php
+++ src/controllers/SampleAdminController.php
@@ -6,18 +6,17 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Source\Ad\Models\Samples;
 
-Class SampleAdminController extends Controller
-{
-	public $data_view = array();
-	private $obj_sample = NULL;
+Class SampleAdminController extends Controller {
 
-	public function __construct() {
+    public $data_view = array();
+    private $obj_sample = NULL;
+
+    public function __construct() {
         $this->obj_sample = new Samples();
     }
 
-    public function index(Request $request)
-    {
-    	$params = $request->all();
+    public function index(Request $request) {
+        $params = $request->all();
 
         $list_sample = $this->obj_sample->get_samples($params);
 
@@ -29,5 +28,4 @@ Class SampleAdminController extends Controller
         return view('ad::admin.sample.sample_index', $this->data_view);
     }
 
-    
 }
