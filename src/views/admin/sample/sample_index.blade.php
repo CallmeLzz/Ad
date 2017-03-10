f442597228d63f17b5583682c246867cd4031bf1
Nhân Võ <tannhan113@gmail.com>
2017-03-10 20:22:41 +0700

    Fix

--- src/views/admin/sample/sample_index.blade.php
+++ src/views/admin/sample/sample_index.blade.php
@@ -1,26 +1,26 @@
 @extends('ad::admin.masterpage')
 
 @if(isset($samples))
-	@section('title', 'Admin Page - List')
+	@section('title', 'Admin Sample - List')
 	@section('main-content')
 	    @include('ad::admin.sample.sample_list')
 	@stop
 @endif
 
 @if(isset($samples_edit))
-	@section('title', 'Admin Page - Edit-Add')
+	@section('title', 'Admin Sample  - Edit')
 	@section('main-content')
 		@include('ad::admin.sample.sample_edit')
 	@stop
 @endif
 
 @if(!isset($samples_edit))
-	@section('title', 'Admin Page - Edit-Add')
+	@section('title', 'Admin Sample  - Add')
 	@section('main-content')
 		@include('ad::admin.sample.sample_add')
 	@stop
 @endif
 
 @section('right-menu')
-	@include('ad::admin.sample.sample_search')
+		@include('ad::admin.sample.sample_search')
 @stop
--- src/views/admin/sample_category/sample_category_index.blade.php
+++ src/views/admin/sample_category/sample_category_index.blade.php
@@ -1,21 +1,21 @@
 @extends('ad::admin.masterpage')
 
 @if(isset($samples_category))
-	@section('title', 'Admin Page - List')
+	@section('title', 'Admin Categories - List')
 	@section('main-content')
 	    @include('ad::admin.sample_category.sample_category_list')
 	@stop
 @endif
 
 @if(isset($samples_category_edit))
-	@section('title', 'Admin Page - Edit-Add')
+	@section('title', 'Admin Categories - Edit')
 	@section('main-content')
 		@include('ad::admin.sample_category.sample_category_edit')
 	@stop
 @endif
 
 @if(!isset($samples_category_edit))
-	@section('title', 'Admin Page - Edit-Add')
+	@section('title', 'Admin Categories - Add')
 	@section('main-content')
 		@include('ad::admin.sample_category.sample_category_add')
 	@stop
