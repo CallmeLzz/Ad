ece7af462e8746d3a415eaf8df3f3e2111d329e0
hao2564 <nhathaonguyen96@gmail.com>
2017-03-15 07:58:17 +0700

    fix resize images
    fix resize images

--- src/views/admin/image/image_list.blade.php
+++ src/views/admin/image/image_list.blade.php
@@ -18,7 +18,7 @@
             @foreach($images as $value)
             <tr>
                 <td>{{ $value['img_id'] }}</td>
-                <td><img src='{{ asset("images/$value->img_name") }}' style="width:100%;">{{ $value->img_name}} </td>
+                <td><img src='{{ asset("images/$value->img_name") }}' style="">{{ $value->img_name}} </td>
                 <td>
                     <a href="{{ URL::route('admin_image.edit', ['id' => $value->img_id]) }}" class="btn btn-info pull left" style="margin-right:3px;">Edit</a>
                     <a href="{!! URL::route('admin_image.delete',['id' =>  $value->img_id, '_token' => csrf_token()]) !!}" class="btn btn-danger pull left submitDelete" source="">Delete</a>
